f114fed39e5733ce9d6a337db6e1dbf0f18f0b22
vinay_l <vinay_l@PSL-8K527L3>
2022-08-22 14:42:47 +0530

    Add project files.

---
+++ Movie e-commerce website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movie_e_commerce_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
